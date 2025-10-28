11af4e1a7c1e90d8c85396c4fe1c6e52fa7d4e75
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2025-10-28 17:25:49 -0300

    Feat: Criar a classe storage que vai salvar os dados no localstorage

---
+++ src/modules/Storage.js
@@ -0,0 +1,55 @@
+import Todo from "./Todo";
+import Project from "./Project";
+import TodoList from "./TodoList";
+
+export default class Storage{
+    static saveTodoList(data){
+        localStorage.setItem('todoList', JSON.stringify(data));
+    }
+
+    static getTodoList() {
+        const todoList = Object.assign(
+        new TodoList(),
+        JSON.parse(localStorage.getItem('todoList'))
+        )
+
+        todoList.setProjects(
+        todoList
+            .getProjects()
+            .map((project) => Object.assign(new Project(), project))
+        )
+
+        todoList
+        .getProjects()
+        .forEach((project) =>
+            project.setTodos(
+            project.getTodos().map((todo) => Object.assign(new Todo(), todo))
+            )
+        )
+        return todoList
+    }
+
+    static addTodoList(project){
+        const todoList = Storage.getTodoList();
+        todoList.addProject(project);
+        Storage.saveTodoList(todoList);
+    }
+
+    static deleteProject(project){
+        const todoList = Storage.getTodoList();
+        todoList.deleteProject(project);
+        Storage.saveTodoList(todoList);
+    }
+
+    static addTodo(projectName, todo){
+        const todoList = Storage.getTodoList();
+        todoList.getProject(projectName).addTodo(todo);
+        Storage.saveTodoList(todoList);
+    }
+
+    static deleteTodo(projectName, todo){
+        const todoList = Storage.getTodoList();
+        todoList.getProject(projectName).deleteTodo(todo);
+        Storage.saveTodoList(todoList);
+    }
+}
